2c18b610bf7f0b7eb2e66b716e32b5290a409f7c
unknown <tavoywalls@gmail.com>
2022-05-25 19:00:34 -0400

    Made game work with fakeCoin bonus

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin){
+        this.name = name;
+        this.timesPlayed = 0;
+        this.isFakeCoin = isFakeCoin;
+    }
+
+    playGame(betAmount){
+        this.timesPlayed++
+
+        let winnings = betAmount * (this.timesPlayed + 1)
+
+        if(Math.random() <= 0.5 || this.isFakeCoin){
+            console.log(this.name + " Wins!")
+        } else {
+            console.log("Player Wins " + winnings + "!!")
+        }
+    }
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU Casino", false);
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
 myCasino.playGame(25);
 myCasino.playGame(35);
 
-// BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
 /*
